695e483fd6917b66c3056a6ec3401fbefeb248d2
“ykkwon” <ykkwon@gruter.com>
2024-02-16 15:43:30 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(),
     # packages=find_packages(where='src'),
     # package_dir={'': 'src'},
-    packages=['OpenDartReader'],
+    # packages=['OpenDartReader'],
     # author="https://github.com/FinanceData/OpenDartReader",
     # py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     zip_safe=False,
