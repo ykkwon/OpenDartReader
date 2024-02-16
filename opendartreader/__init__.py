4ec2eecc040089ae6b943f01a6b1b31e0f6f80b9
“ykkwon” <ykkwon@gruter.com>
2024-02-16 15:39:49 +0900

    setup.py

--- opendartreader/__init__.py
+++ opendartreader/__init__.py
@@ -3,7 +3,7 @@
 import sys
 from .dart import *
 
-__version__ = '0.2.2'
-__all__ = ['__version__', 'OpenDartReader']
+# __version__ = '0.2.2'
+# __all__ = ['__version__', 'OpenDartReader']
 
-sys.modules['OpenDartReader'] = dart.OpenDartReader
+# sys.modules['OpenDartReader'] = dart.OpenDartReader
